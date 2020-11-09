ae4bf4e09366c696d66577a69ba259f30c143414
Jeff Trin <trin.jeff@yahoo.com>
2020-11-08 19:52:07 -0700

    brand new

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './layout.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Layout() {
+  return (
+      <div>My name is Jeff</div>
+  )
+}
